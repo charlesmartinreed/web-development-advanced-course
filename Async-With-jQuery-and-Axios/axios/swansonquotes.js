9de61c003a5d380c645334f1de2a000b7f5bf810
charlesmartinreed <charlesmartinreed@icloud.com>
2019-03-20 13:52:45 -0500

    Complete inital (very rough) version of Ron Swanson Quote Generator exercise.

---
+++ Async-With-jQuery-and-Axios/axios/swansonquotes.js
@@ -0,0 +1,82 @@
+// DOM variables
+const axiosBtn = document.querySelector('#axios');
+const jQueryBtn = $("#jQuery");
+const fetchBtn = document.querySelector('#fetch');
+const xhrBtn = document.querySelector('#xhr');
+
+let quoteP = document.querySelector('#quote');
+
+// event listeners
+axiosBtn.addEventListener('click', function() {
+	axios.get('https://ron-swanson-quotes.herokuapp.com/v2/quotes')
+	.then(displayAxiosQuote)
+	.catch(displayErrors)
+});
+
+jQueryBtn.click(function() {
+	$.getJSON('https://ron-swanson-quotes.herokuapp.com/v2/quotes')
+	.then(displayjQueryQuote)
+	.catch(displayErrors)
+});
+
+fetchBtn.addEventListener('click', function() {
+	fetch('https://ron-swanson-quotes.herokuapp.com/v2/quotes')
+	.then(parseFetch)
+	.then(displayFetchQuote)
+})
+
+xhrBtn.addEventListener('click', function() {
+	var request = new XMLHttpRequest();
+
+	request.onreadystatechange = function() {
+		if(request.readyState == 4 && request.status === 200) {
+			parseXHRrequest(request.responseText);
+		}
+	}
+
+	request.open('GET', 'https://ron-swanson-quotes.herokuapp.com/v2/quotes')
+
+	request.send()
+})
+
+// XHR
+function parseXHRrequest(req) {
+	let quote = JSON.parse(req)[0];
+	displayXHRQuote(quote);
+}
+
+function displayXHRQuote(quote) {
+	quoteP.innerText = quote;
+}
+
+// FETCH
+function parseFetch(res) {
+	return res.json()
+}
+
+function displayFetchQuote(data) {
+	let quote = data[0];
+	quoteP.innerText = quote;
+}
+
+// AXIOS
+function displayAxiosQuote(res) {
+	let quote = res.data[0];
+	quoteP.innerText = quote;
+}
+
+// JQUERY
+function displayjQueryQuote(quote) {
+	// console.log(quote[0]);
+	$("#quote").text(quote[0]);
+}
+
+function displayErrors(error) {
+	if(error.response) {
+		console.log("Response error", error.response.status);
+	} else if (error.request) {
+		console.log("Request error");
+	} else {
+		console.log("General error", error.message);
+	}
+}
