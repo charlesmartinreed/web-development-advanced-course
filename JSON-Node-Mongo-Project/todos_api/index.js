30432f8f8f3d963d766daa9c637d0947977e7c25
charlesmartinreed <charlesmartinreed@icloud.com>
2019-03-27 13:30:57 -0500

    Create /public, app.css, app.js. Create /views, create index.html.

--- JSON-Node-Mongo-Project/todos_api/index.js
+++ JSON-Node-Mongo-Project/todos_api/index.js
@@ -10,11 +10,18 @@ var todoRoutes = require('./routes/todos');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+// when we sendFile, this will allows us to reference the full path of our served files
+app.use(express.static(__dirname + '/views'));
+
+// this allows us to use our css, app, etc.
+app.use(express.static(__dirname + '/public'))
+
 app.get('/', function(req, res) {
 	// res.send("Hello world! I'm an express app!");
 	// res.send({message: "Hi, this is from a JS Object!"});
 	// res.json({message: "Hi, this is from a JS Object!"});
-	res.send("HELLO FROM THE ROOT ROUTE!");
+	// res.send("HELLO FROM THE ROOT ROUTE!");
+	res.sendFile("index.html");
 });
 
 // this is how we tell our app to use specific routes
