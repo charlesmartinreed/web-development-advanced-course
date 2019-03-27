c151ac0259e4da7fc0222288ca7f0c3b49bbd316
charlesmartinreed <charlesmartinreed@icloud.com>
2019-03-27 14:03:36 -0500

    Write initial AJAX for retreiving tasks/updating DOM, using jQuery.

--- JSON-Node-Mongo-Project/todos_api/public/app.js
+++ JSON-Node-Mongo-Project/todos_api/public/app.js
@@ -1,1 +1,16 @@
+// WHEN THE DOM HAS LOADED
+$(document).ready(function() {
+	// request our todos from our apis - /api/todos
+	$.getJSON("/api/todos")
+		.then(addTodos)
+});
 
+function addTodos(todos) {
+	todos.forEach(todo => {
+		// make a new li with the todo
+		var newTodo = $('<li class="task">'+todo.name + '</li>');
+
+		// append to the list
+		$('.list').append(newTodo);
+	})
+}
